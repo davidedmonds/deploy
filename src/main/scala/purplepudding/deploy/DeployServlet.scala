74d5fd0efef2b302f2a5c9aa7f0a0e305c39b1ab
David Edmonds <edmonds.d.r@gmail.com>
2016-05-13 07:32:07 +0100

    Deploy: A CD framework focused on speed.

---
+++ src/main/scala/purplepudding/deploy/DeployServlet.scala
@@ -0,0 +1,16 @@
+package purplepudding.deploy
+
+import org.scalatra._
+
+class DeployServlet extends DeployStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
