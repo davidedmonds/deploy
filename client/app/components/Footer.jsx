5be7d8766843a296b485a50160ec817ffc4ea56b
David Edmonds <edmonds.d.r@gmail.com>
2016-06-02 06:16:07 +0100

    Add license file to root, and link to source in footer.

--- client/app/components/Footer.jsx
+++ client/app/components/Footer.jsx
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        Deploy v1.
+        Deploy v1. <a href="https://github.com/davidedmonds/deploy">Source on Github</a>
       </footer>
     );
   }
