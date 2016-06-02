a3de461f2286e945235e3eb1162884a9c0d1bc9c
David Edmonds <edmonds.d.r@gmail.com>
2016-06-02 06:31:34 +0100

    Add Copyright notice to all files, and prune remaining ones

--- server/src/main/resources/db/migration/V1__Create_Pipeline_Table.sql
+++
@@ -1,4 +0,0 @@
-create table pipeline (
-    ID int auto_increment,
-    NAME varchar(100) not null
-)
--- server/src/main/resources/db/migration/V2__Create_Stage_Table.sql
+++
@@ -1,5 +0,0 @@
-create table stage (
-    ID int auto_increment,
-    NAME varchar(100) not null,
-    PIPELINE_ID int not null
-)
